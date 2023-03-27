edd865b5d8fa287fbb636d777a566abf8794bce5
Miranda Cascione <miranda.cascione@gmail.com>
2023-03-26 17:57:08 -0700

    moved BrowserRouter to index.js and removed from app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <>
       <h1>Bloomtech Eats</h1>
       <p>Fuel Your Coding Session with Pizza</p>
-    </BrowserRouter>
+    </>
   );
 };
 export default App;
