e7e15f44a9adbcca93860bf78f03218b6c28cc20
Miranda Cascione <miranda.cascione@gmail.com>
2023-03-27 18:24:28 -0700

    updated styling to make the Order Now button more prominent on the Home page and the Form more prominent on the Form component

--- src/App.js
+++ src/App.js
@@ -6,14 +6,12 @@ import Form from "./components/Form";
 
 const App = () => {
   return (
-      <>
-        <header className="App-header">
+      <>  
           <nav className="Top-nav">
           <Link to="/" className="App-link">Home</Link>
           </nav> 
           <h1>Bloomtech Eats</h1>
           <p>Fuel Your Coding Session with Pizza</p>
-        </header>
         <Routes>
             <Route path="/" element={<Homepage />}/>
             <Route path="/pizza" element={<Form />}/>  
